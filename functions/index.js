e0867274097386fee265dd627d9d506472618992
Carlos Sechi <carlos.sechi@zrp.com.br>
2021-06-29 23:18:00 -0300

    Add firestore backup function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,26 @@
 const functions = require('firebase-functions');
+const firestore = require('@google-cloud/firestore');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const client = new firestore.v1.FirestoreAdminClient();
+
+const bucket = 'gs://diabeto-7ba34.appspot.com';
+
+exports.scheduledFirestoreExport = functions.pubsub
+  .schedule('0 0 1 * *')
+  .onRun(async () => {
+    const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
+    const databaseName = client.databasePath(projectId, '(default)');
+
+    try {
+      const [response] = await client.exportDocuments({
+        name: databaseName,
+        outputUriPrefix: bucket,
+        collectionIds: ['users']
+      });
+
+      console.log(`Operation Name: ${response['name']}`);
+    } catch (error) {
+      console.error(error);
+      throw new Error('Export operation failed')
+    }
+  });
